ca57ac6467e14bcf58516167195c2dc4acd29b79
Carrotw <91509157+MetTheCarrot@users.noreply.github.com>
2022-11-05 17:31:44 -0500

    jarvis llama a yosstin.jpg

--- SAIU_DATOS/admin.py
+++ SAIU_DATOS/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from SAIU_DATOS.models import Agosto, Septiembre, Octubre, Noviembre
 # Register your models here.
 
-class extraInfor(admin.ModelAdmin):
-    list_display = ("name", "description")
+# class extraInfor(admin.ModelAdmin):
+#     list_display = ("name", "description")
 
 admin.site.register(Agosto)
 admin.site.register(Septiembre)
--- SAIU_SRC/views.py
+++ SAIU_SRC/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from SAIU_DATOS.models import Agosto, Septiembre, Octubre, Noviembre
+
 
 @login_required
 def base(request):
-    return render(request, "informacion.html")
+    a = []
+    b = []
+    c = []
+    d = []
+    for i in (Agosto.objects.all()):
+        a.append(i.primeraSemana)
+        a.append(i.segundaSemana)
+        a.append(i.terceraSemana)
+        a.append(i.cuartaSemana)
+    for i in (Septiembre.objects.all()):
+        b.append(i.primeraSemana)
+        b.append(i.segundaSemana)
+        b.append(i.terceraSemana)
+        b.append(i.cuartaSemana)
+    for i in (Octubre.objects.all()):
+        c.append(i.primeraSemana)
+        c.append(i.segundaSemana)
+        c.append(i.terceraSemana)
+        c.append(i.cuartaSemana)
+    for i in (Noviembre.objects.all()):
+        d.append(i.primeraSemana)
+        d.append(i.segundaSemana)
+        d.append(i.terceraSemana)
+        d.append(i.cuartaSemana)
+    print(f'Agosto: {a}')
+    print(f'Septiembre: {b}')
+    print(f'Octubre: {c}')
+    print(f'Noviembre: {d}')
+    return render(request, "informacion.html", {"a": a, "b": b, "c": c, "d": d})
